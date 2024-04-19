31d158bd5a580c6d0cbf0e4dd7c0ad2a8269c71a
fruitbox12 <dylanwong007@gmail.com>
2024-04-19 13:40:35 -0700

    Update index.ts

--- graphql/api/index.ts
+++ graphql/api/index.ts
@@ -1,35 +1,31 @@
-import { ApolloServer } from 'apollo-server-express'
-import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
-import http from 'http'
-import express from 'express'
-import cors from 'cors'
-import { connectToDatabase } from '../src/database'
-import { Express } from 'express-serve-static-core'
-import { TYPE_DEFS } from '../src/type-defs/type-defs'
-import { RESOLVERS } from '../src/resolvers/resolvers'
-
-export const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-const httpServer = http.createServer(app)
-
-const startApolloServer = async(app: Express, httpServer: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>) => {
-  await connectToDatabase()
-
-  const server = new ApolloServer({
-    typeDefs: TYPE_DEFS,
-    resolvers: RESOLVERS,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageGraphQLPlayground()],
-  })
-
-  await server.start()
-  server.applyMiddleware({ path: '/graphql', app })
-}
-
-startApolloServer(app, httpServer)
-
-app.listen(process.env.PORT || 4000, () => console.info('Server started'))
-
-export default httpServer
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const apollo_server_express_1 = require("apollo-server-express");
+const apollo_server_core_1 = require("apollo-server-core");
+const http_1 = __importDefault(require("http"));
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const database_1 = require("../src/database");
+const type_defs_1 = require("../src/type-defs/type-defs");
+const resolvers_1 = require("../src/resolvers/resolvers");
+exports.app = (0, express_1.default)();
+exports.app.use((0, cors_1.default)());
+exports.app.use(express_1.default.json());
+const httpServer = http_1.default.createServer(exports.app);
+const startApolloServer = async (app, httpServer) => {
+    await (0, database_1.connectToDatabase)();
+    const server = new apollo_server_express_1.ApolloServer({
+        typeDefs: type_defs_1.TYPE_DEFS,
+        resolvers: resolvers_1.RESOLVERS,
+        plugins: [(0, apollo_server_core_1.ApolloServerPluginDrainHttpServer)({ httpServer }), (0, apollo_server_core_1.ApolloServerPluginLandingPageGraphQLPlayground)()],
+    });
+    await server.start();
+    server.applyMiddleware({ path: '/graphql', app });
+};
+startApolloServer(exports.app, httpServer);
+exports.app.listen(process.env.PORT || 4000, () => console.info('Server started'));
+exports.default = httpServer;
