82a409afca12a08057168a53e660ffe667b8cbe1
fruitbox12 <dylanwong007@gmail.com>
2024-04-23 01:38:00 -0700

    Update database.ts

--- graphql/src/database.ts
+++ graphql/src/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export async function connectToDatabase() {
   try {
-    const localConnectionString = 'mongodb://root:root@localhost:27017/aleph-indexer?authSource=admin'
+    const localConnectionString = 'mongodb+srv://dylan:0jxwH1S7tdOw2sK8@cluster0.zbnre8w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     await mongoose.connect(process.env.DB_URL || localConnectionString)
 
     console.log('Successfully connected to MongoDB')
