4e05592c5b69af98fc7ba37e8ba76ee19560bede
Brendan Giberson <bcgiberson@gmail.com>
2024-05-03 10:19:28 -0400

    Read mongo user/pass from env

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import {
 import { MongoDBDatabase } from './mongo-database'
 import { startIndexingVault } from './index-events'
 
-const localConnectionString = 'mongodb://root:root@localhost:27017/aleph-indexer?authSource=admin'
+const localConnectionString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`
 
 //TODO would be nice to read the starting block in automagically
-const mongoDB = new MongoDBDatabase(process.env.DB_URL || localConnectionString, 'aleph-indexer', Number(process.env.STARTING_BLOCK) || 1)
+const mongoDB = new MongoDBDatabase(process.env.DB_URL || localConnectionString,
+                                    process.env.DB_NAME || 'aleph-indexer', 
+                                    Number(process.env.STARTING_BLOCK) || 1)
     
 processor.run(mongoDB, async ctx => {
     console.log("PROCESSING LOOP")
